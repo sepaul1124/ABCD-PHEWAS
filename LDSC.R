13f47eaf3cd8b5b42c7326528a936cda89206a14
sepaul1124 <100047113+sepaul1124@users.noreply.github.com>
2022-12-27 18:06:03 -0600

    Update LDSC.R

--- LDSC.R
+++ LDSC.R
@@ -1,8 +1,8 @@
-traits <- c("cEF.sumstats.gz", "intelligence.sumstats.gz", "eduattain.sumstats.gz")
-sample.prev <- c(NA, NA, NA, NA)
-population.prev <- c(NA, NA, NA, NA)
+traits <- c("adhd.sumstats.gz", "asd.sumstats.gz", "gad.sumstats.gz", "ocd.sumstats.gz", "pau.sumstats.gz", "an.sumstats.gz", "bip.sumstats.gz", "mdd.sumstats.gz", "ptsd.sumstats.gz", "scz.sumstats.gz", "ts.sumstats.gz")
+sample.prev <- c(.5, .5, NA, .5, NA, .5, .5, .5, .5, .5, .5)
+population.prev <- c(.03, .006, NA, .01, NA, .002, .01, .16, .08, .01, .01)
 ld <- "eur_w_ld_chr/"
 wld <- "eur_w_ld_chr/"
-trait.names <- c("cEF", "int", "eduattain")
-LDSCoutput_COG <- ldsc(traits=traits, sample.prev=sample.prev, population.prev=population.prev, ld=ld, wld=wld, trait.names=trait.names)
-save(LDSCoutput_COG, file = "LDSCoutput_COG.RData")
+trait.names <- c("adhd", "asd", "gad", "ocd", "pau", "an", "bip", "mdd", "ptsd", "scz", "ts")
+LDSCoutput_CD <- ldsc(traits=traits, sample.prev=sample.prev, population.prev=population.prev, ld=ld, wld=wld, trait.names=trait.names)
+save(LDSCoutput_CD, file = "LDSCoutput_CD.RData")
