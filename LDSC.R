1ecd5ed46cd2883172400999eb9fb969a0486b79
sepaul1124 <100047113+sepaul1124@users.noreply.github.com>
2024-03-30 21:56:38 -0500

    Update LDSC.R

--- LDSC.R
+++ LDSC.R
@@ -1,8 +1,12 @@
-traits <- c("adhd.sumstats.gz", "asd.sumstats.gz", "gad.sumstats.gz", "ocd.sumstats.gz", "pau.sumstats.gz", "an.sumstats.gz", "bip.sumstats.gz", "mdd.sumstats.gz", "ptsd.sumstats.gz", "scz.sumstats.gz", "ts.sumstats.gz")
-sample.prev <- c(.5, .5, NA, .5, NA, .5, .5, .5, .5, .5, .5)
-population.prev <- c(.03, .006, NA, .01, NA, .002, .01, .16, .08, .01, .01)
+traits <- c("adhd.sumstats.gz", "asd.sumstats.gz", "gad.sumstats.gz", "ocd.sumstats.gz", "pau.sumstats.gz", "an.sumstats.gz", 
+            "bip.sumstats.gz", "mdd.sumstats.gz", "ptsd.sumstats.gz", "scz.sumstats.gz", "ts.sumstats.gz")
+sample.prev <- c(.5, .5, NA, .5, NA, .5, 
+                 .5, .5, .5, .5, .5)
+population.prev <- c(.03, .006, NA, .01, NA, .002, 
+                     .01, .16, .08, .01, .01)
 ld <- "eur_w_ld_chr/"
 wld <- "eur_w_ld_chr/"
-trait.names <- c("adhd", "asd", "gad", "ocd", "pau", "an", "bip", "mdd", "ptsd", "scz", "ts")
+trait.names <- c("adhd", "asd", "gad", "ocd", "pau", "an", 
+                 "bip", "mdd", "ptsd", "scz", "ts")
 LDSCoutput_CD <- ldsc(traits=traits, sample.prev=sample.prev, population.prev=population.prev, ld=ld, wld=wld, trait.names=trait.names)
 save(LDSCoutput_CD, file = "LDSCoutput_CD.RData")
